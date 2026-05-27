e077dc679c11a8a15597585ba61dacbd2a60d6dd
mariam130396-a11y <mariam130396@gmail.com>
2026-05-27 22:14:19 +0200

    Update monrocod.py

--- monrocod.py
+++ monrocod.py
@@ -3,21 +3,30 @@ from PIL import Image
 image = Image.open ("monro.jpg")
 red, green, blue = image.split()
 
-red_coordinates = (200, 0, red.width, red.height)
-red_cropped = red.crop (red_coordinates) 
+red_coordinates_1 = (200, 0, red.width, red.height)
+red_cropped_1 = red.crop (red_coordinates_1) 
 
-red_coordinates = (100, 0, red.width-100, red.height) 
-red_cropped = red.crop (red_coordinates) 
+red_coordinates_2 = (100, 0, red.width-100, red.height) 
+red_cropped_2 = red.crop (red_coordinates_2) 
 
-blue_coordinates = (0, 0, blue.width - 200, blue.height)
-biue_cropped = blue.crop (blue_coordinates)
+redmix = Image.blend (red_cropped_1, red_cropped_2 , 0.5)
 
-blue_coordinates = (100, 0, blue.width-100, blue.height) 
-biue_cropped = blue.crop (blue_coordinates)
+blue_coordinates_1 = (0, 0, blue.width - 200, blue.height)
+blue_cropped_1 = blue.crop (blue_coordinates_1)
 
-green_coordinates = (100, 0, green.width-100, green.height) 
+blue_coordinates_2 = (100, 0, blue.width-100, blue.height) 
+blue_cropped_2 = blue.crop (blue_coordinates_2)
+
+bluemix = Image.blend (blue_cropped_1, blue_cropped_2 , 0.5)
+
+green_coordinates_1 = (100, 0, green.width-100, green.height) 
+green_cropped_1 = green.crop (green_coordinates_1)
 
 result= Image.merge ("RGB" , ( red , green , blue  ))
 
 image.thumbnail ((80, 80))
 image.save ("smallfinalmonroo.jpg")
+
+
+
+
